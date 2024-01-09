7034a25c4e8b2ddd0bf53c670df10c4c92eca86b
mikun2001 <abhilashdash2023@gmail.com>
2024-01-09 21:17:34 +0530

    added CRUD for hotel without authentication

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,24 +1,28 @@
 import express, { Router } from "express";
 import hotel from "../models/hotel.js";
+import {
+	createHotel,
+	deleteHotel,
+	getHotel,
+	getHotels,
+	updateHotel,
+} from "../controllers/hotel.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.send("hello, this is hotel end point.");
-});
+//CREATE
+router.post("/", createHotel);
+
+//UPDATE
+router.put("/:id", updateHotel);
+
+//DELETE
+router.delete("/:id", deleteHotel);
 
 //GET
+router.get("/find/:id", getHotel);
+
 //GET ALL
-//CREATE
-router.post("/", async (req, res) => {
-	const newHotel = new hotel(req.body);
-	try {
-		const savedHotel = await newHotel.save();
-		res.status(200).json(savedHotel);
-	} catch (error) {
-		res.status(500).json(error);
-	}
-});
-//UPDATE
-//DELETE
+router.get("/", getHotels);
+
 export default router;
